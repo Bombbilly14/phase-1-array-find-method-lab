0fe54e47bce9d9cacd306b6c418b8372eba2be15
William German <william.j.german2@gmail.com>
2022-11-01 15:50:00 -0700

    commit tree 972f6daf101cf15bdf55f67d5cc2527bac52462c

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+// code your solution 
+
+function superbowlWin(games) {
+    const wonGame=games.find((game)=>{
+        return game.result === "W";
+    
+    });
+    if (wonGame) {
+    return wonGame.year;
+    }
+
+}
